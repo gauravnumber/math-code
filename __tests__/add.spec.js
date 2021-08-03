f1c170b96a2c5bded0efa3a8291ece194c1c4bb2
Gaurav Singh <gauravnumber@gmail.com>
2021-08-03 19:31:14 +0530

    jest working with jest.config.json

--- __tests__/add.spec.js
+++ __tests__/add.spec.js
@@ -2,13 +2,14 @@
 import { add } from '../js/add.js'
 
 const array = [
-	[1, 2, 3],
-	[34, 12, 46],
-	[12, 12, 24]
+	[1, 2, "3"],
+	[34, 12, "46"],
+	[12, 12, "24"]
 ]
 
 test("addition", () => {
 	expect(add(2, 4)).toEqual("6")
+	// expect(2).toEqual(2)
 })
 // for (let arr of array) {
 // 	Deno.test(`${arr[0]} + ${arr[1]} = ${arr[2]}`, () => {
