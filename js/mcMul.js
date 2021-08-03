e3294610ecc68eb564c5d24a56c1845f15c87621
Gaurav Singh <gauravnumber@gmail.com>
2021-08-03 08:44:23 +0530

    mcMul done

--- js/mcMul.js
+++ js/mcMul.js
@@ -1,8 +1,9 @@
 import { mcMulSection } from './mcMulSection.js'
-export function mcMul(){
+
+export function mcMul() {
 	var third = 1;
-	for( var i = 0, j = arguments.length; i < j; i++ ){
-		third = mcMulSection( arguments[ i ], third );
+	for (var i = 0, j = arguments.length; i < j; i++) {
+		third = mcMulSection(arguments[i], third);
 	}
 	return third;
 }
--- js/mcMulOne.js
+++ js/mcMulOne.js
@@ -1,24 +1,24 @@
-export function mcMulOne( number, one ){
+export function mcMulOne(number, one) {
 	var lengthno,
 		i,
 		numberLastIndex,
 		third;
 	third = [];
-	number = String( number );
-	one = Number( one );
+	number = String(number);
+	one = Number(one);
 	lengthno = number.length;
-	number = number.split( "" );
+	number = number.split("");
 	numberLastIndex = lengthno - 1;
-	for( i = 0; i < lengthno; i++, numberLastIndex-- ) {
-		third[i] = isNaN( third[i] ) ? 0 : third[i];
-		third[i] = Number( third[i] ) + Number( number[ numberLastIndex ] ) * one;
-		third[i] = String( third[i] );
-		if( third[i].length == 2 ) {
-			third[ i + 1 ] = third[i].charAt( 0 );
-			third[ i ] = third[i].charAt( 1 );
+	for (i = 0; i < lengthno; i++, numberLastIndex--) {
+		third[i] = isNaN(third[i]) ? 0 : third[i];
+		third[i] = Number(third[i]) + Number(number[numberLastIndex]) * one;
+		third[i] = String(third[i]);
+		if (third[i].length == 2) {
+			third[i + 1] = third[i].charAt(0);
+			third[i] = third[i].charAt(1);
 		}
 	}
 	third = third.reverse();
-	third = third.join( "" );
+	third = third.join("");
 	return third;
 }
