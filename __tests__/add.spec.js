af329e4537d3016fc12d4957ede8c701f8bdc2db
Gaurav Singh <gauravnumber@gmail.com>
2021-08-04 11:26:37 +0530

    test.each worked

--- __tests__/add.spec.js
+++ __tests__/add.spec.js
@@ -11,6 +11,14 @@ test("addition", () => {
 	expect(add(2, 4)).toEqual("6")
 	// expect(2).toEqual(2)
 })
+
+test.each([
+	[1, 1, 2],
+	[2, 1, 3],
+	[1, 4, 5]
+])('.add(%i, %i)', (a, b, expected) => {
+	expect(a + b).toBe(expected)
+})
 // for (let arr of array) {
 // 	Deno.test(`${arr[0]} + ${arr[1]} = ${arr[2]}`, () => {
 // 		equal(add(arr[0], arr[1]), arr[2])
