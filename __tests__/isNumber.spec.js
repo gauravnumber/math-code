40ea4ccf30e6b69915b572e5643a2d52486d5a75
Gaurav Singh <gauravnumber@gmail.com>
2021-08-11 21:57:04 +0530

    isNumber done

---
+++ __tests__/isNumber.spec.js
@@ -0,0 +1,21 @@
+import isNumber from '../js/.internal/isNumber.js'
+
+describe('isNumber', () => {
+  const array = [
+    ["0", true],
+    ["0.0", true],
+    ["24233", true],
+    ["00003", true],
+    ["00340034", true],
+    ["34.54", true],
+    ["-23", true],
+    ["-34-34", false],
+    ["3.4.34", false],
+    ["34ad", false]
+  ]
+
+  it.each(array)('isNumber(%s)', (a, expected) => {
+    expect(isNumber(a)).toBe(expected)
+  })
+
+});
