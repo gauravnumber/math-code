6c45a6f5b0e14c221edc6a0fc30303dfa793749a
Gaurav Singh <gauravnumber@gmail.com>
2021-08-16 16:56:06 +0530

    divisble test easy, negative, decimal

--- __tests__/mcDiv.spec.js
+++ __tests__/mcDiv.spec.js
@@ -1,12 +1,33 @@
 import { mcDiv } from '../js/mcDiv.js'
 
 describe('Division', () => {
-  const array = [
+  const easy = [
     ["15", "3", "5"],
     ["12", "2", "6"],
   ]
 
-  it.each(array)("Divide %s/%s", (a, b, expected) => {
+  const negative = [
+    ["-15", "3", "-5"],
+    ["12", "-2", "-6"],
+    ["-12", "-2", "6"],
+  ]
+
+  const decimal = [
+    ["10", "4", "2.5"],
+    ["9.25", "5", "1.85"],
+    ["8.5", "5", "1.7"],
+    ["3.657", "1.59", "2.3"],
+  ]
+
+  it.each(easy)("Easily divisible number %s/%s", (a, b, expected) => {
+    expect(mcDiv(a, b)).toBe(expected);
+  })
+
+  it.each(negative)("Negative %s/%s", (a, b, expected) => {
+    expect(mcDiv(a, b)).toBe(expected);
+  })
+
+  it.each(decimal)("Decimal %s/%s", (a, b, expected) => {
     expect(mcDiv(a, b)).toBe(expected);
   })
 });
