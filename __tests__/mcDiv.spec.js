0e6b63a56ba69920ea6bb9a7242a808d57accd99
Gaurav Singh <gauravnumber@gmail.com>
2021-08-16 16:41:57 +0530

    test of mcDiv.js worked

--- __tests__/mcDiv.spec.js
+++ __tests__/mcDiv.spec.js
@@ -2,7 +2,8 @@ import { mcDiv } from '../js/mcDiv.js'
 
 describe('Division', () => {
   const array = [
-    ["15", "3", "5"]
+    ["15", "3", "5"],
+    ["12", "2", "6"],
   ]
 
   it.each(array)("Divide %s/%s", (a, b, expected) => {
