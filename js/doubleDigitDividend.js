de1d95e0025b20591320224b862f926fd376193f
Gaurav Singh <gauravnumber@gmail.com>
2021-08-17 18:38:49 +0530

    First I need solid all math operation

--- js/doubleDigitDividend.js
+++ js/doubleDigitDividend.js
@@ -1,5 +1,6 @@
 import isDecimal from './.internal/isDecimal.js'
 import { isGt } from './isGt.js'
+import { isEq } from './isEq.js'
 import { mul } from './mul.js'
 import { sub } from './sub.js'
 
@@ -11,21 +12,30 @@ const doubleDigitDividend = (dividend, divisor) => {
 
   dividend = dividend.split("")
   divisor = divisor.split("")
-  console.log('dividend, divisor', dividend, divisor)
+  // console.log('dividend, divisor', dividend, divisor)
 
   if (!isDecimal(dividend) && !isDecimal(divisor)) {
     if (isGt(dividend, divisor)) {
-      quotient = divisibleFor(dividend, divisor)
+      quotient = divisibleFor(dividend.join(""), divisor.join(""))
+      console.log('quotient', quotient)
+      console.debug;
+
       mulTemp = mul(divisor, quotient)
       remainder = sub(dividend, mulTemp)
+
+      console.log('dividend', dividend)
+      
       // mul = divisor * quotient
       // remainder = dividend - mulTemp
       console.log('quotient, mulTemp, remainder', quotient, mulTemp, remainder)
+      console.debug;
 
-      if (remainder === 0) {
+      // if (remainder === 0) {
+      if (isEq(remainder, "0")) {
         return quotient
       } else {
         //? Here is 10 times limit
+        remainder = remainder.split("")
         quotient = singleDigitDiv(remainder, divisor)
         return quotient
       }
