be2bff65dc5343916e2aebecfab3c9d0906df5b7
Gaurav Singh <gauravnumber@gmail.com>
2021-08-17 15:45:45 +0530

    doubleDigitDiv.js added 40/8

--- js/doubleDigitDividend.js
+++ js/doubleDigitDividend.js
@@ -1,20 +1,31 @@
+import isDecimal from './.internal/isDecimal.js'
+import { isGt } from './isGt.js'
+import { mul } from './mul.js'
+import { sub } from './sub.js'
+
+import singleDigitDiv from './singleDigitDiv.js'
+import divisibleFor from './divisibleFor.js'
+
 const doubleDigitDividend = (dividend, divisor) => {
-  let quotient
+  let quotient, mulTemp
 
   dividend = dividend.split("")
   divisor = divisor.split("")
 
   if (!isDecimal(dividend) && !isDecimal(divisor)) {
-    if (isGt(divisor, divisor)) {
+    if (isGt(dividend, divisor)) {
       quotient = divisibleFor(dividend, divisor)
-      mul = divisor * quotient
-      remainder = dividend - mul
+      mulTemp = mul(divisor, quotient)
+      remainder = sub(dividend, mulTemp)
+      // mul = divisor * quotient
+      // remainder = dividend - mulTemp
 
       if (rem === 0) {
         return quotient
       } else {
         //? Here is 10 times limit
-        q = singleDigitDiv(remainder, divisor)
+        quotient = singleDigitDiv(remainder, divisor)
+        return quotient
       }
     }
   }
