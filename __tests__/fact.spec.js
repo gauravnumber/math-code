fde8728d819c6f0f338e396b94653181d74b3762
Gaurav Singh <gauravnumber@gmail.com>
2021-08-20 17:01:07 +0530

    fact done

--- __tests__/fact.spec.js
+++ __tests__/fact.spec.js
@@ -2,10 +2,10 @@ import { fact } from '../js/fact.js'
 
 describe("factorial", () => {
 	const array = [
-		[1, "1"],
-		[4, "24"],
-		[5, "120"],
-		[2, "2"],
+		["1", "1"],
+		["4", "24"],
+		["5", "120"],
+		["2", "2"],
 	]
 
 	it.each(array)('fact(%i)', (a, expected) => {
