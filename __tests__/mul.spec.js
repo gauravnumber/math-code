3e92901ebbbd61cdeb006cc6398017c4d02d33a8
Gaurav Singh <gauravnumber@gmail.com>
2021-08-20 16:55:23 +0530

    mul done

--- __tests__/mul.spec.js
+++ __tests__/mul.spec.js
@@ -2,18 +2,26 @@ import { mul } from '../js/mul.js'
 
 describe("multiplication", () => {
 	const array = [
-		[2, 3, "6"],
-		[566, 13, "7358"],
-		[2, 5, "10"],
+		["2", "3", "6"],
+		["566", "13", "7358"],
+		["2", "5", "10"],
 		// [2, 0, "0"],
 		// [0, -2, "0"],
 		// [-2, 0, "0"],
-		[2, 10, "20"],
-		[12, 12, "144"]
+		["2", "10", "20"],
+		["12", "12", "144"]
 	]
 
 	it.each(array)('mul(%i, %i)', (a, b, expected) => {
 		expect(mul(a, b)).toBe(expected)
 		// expect(mul(a, b)).toEqual(expected)
+	})
+
+	it('two arguments', () => {
+		expect(mul("12243423", "7365273")).toBe("90176152849479");
+	})
+
+	it('multiple arguments', () => {
+		expect(mul("12243423", "7365273", "8748347", "87887908")).toBe("69334091797171062698030512404");
 	})
 })
