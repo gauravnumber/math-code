a91f28d623174deed18593d2ca7bab1f123bb9d8
Gaurav Singh <gauravnumber@gmail.com>
2021-08-20 19:00:38 +0530

    doubleDigitDividend.js not working as expected

--- js/doubleDigitDividend.js
+++ js/doubleDigitDividend.js
@@ -6,6 +6,7 @@ import { sub } from './sub.js'
 
 import singleDigitDiv from './singleDigitDiv.js'
 import divisibleFor from './divisibleFor.js'
+import isNumber from './.internal/isNumber.js'
 
 const doubleDigitDividend = (dividend, divisor) => {
   let quotient, mulTemp, remainder
@@ -18,22 +19,22 @@ const doubleDigitDividend = (dividend, divisor) => {
     if (isGt(dividend, divisor)) {
       quotient = divisibleFor(dividend.join(""), divisor.join(""))
       console.log('quotient', quotient)
-      console.debug;
 
       mulTemp = mul(divisor, quotient)
       remainder = sub(dividend, mulTemp)
 
       console.log('dividend', dividend)
-      
+
       // mul = divisor * quotient
       // remainder = dividend - mulTemp
+      console.log('isNumber(remainder)', isNumber(remainder))
       console.log('quotient, mulTemp, remainder', quotient, mulTemp, remainder)
       console.debug;
 
       // if (remainder === 0) {
       if (isEq(remainder, "0")) {
         return quotient
-      } else {
+      } else if (isNumber(remainder)) {
         //? Here is 10 times limit
         remainder = remainder.split("")
         quotient = singleDigitDiv(remainder, divisor)
