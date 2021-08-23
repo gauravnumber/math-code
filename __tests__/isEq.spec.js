25020615b43e1a1ea1b1bc6287f464f1b80934af
Gaurav Singh <gauravnumber@gmail.com>
2021-08-23 12:34:44 +0530

    isEq test worked

---
+++ __tests__/isEq.spec.js
@@ -0,0 +1,38 @@
+import { isEq } from '../js/isEq.js'
+
+describe('isEq', () => {
+  const easy = [
+    ["050", "50", true],
+    ["3", "3", true],
+    ["12", "2", false],
+    // ["100", "2", "50"],
+    // ["360", "3", "120"],
+    // ["70", "35", "2"],
+  ]
+
+  const negative = [
+    ["-15", "-15", true],
+    ["-012", "-0012", true],
+    ["-12", "-2", false],
+  ]
+
+  const decimal = [
+    ["1.0", "4.0", false],
+    ["9.25", "9.25", true],
+    ["8.5", "8.5000", true],
+    ["3.657", "3.657", true],
+  ]
+
+  it.each(easy)("Easy isEq(%s, %s)", (a, b, expected) => {
+    expect(isEq(a, b)).toBe(expected);
+  })
+
+  it.each(negative)("Negative isEq(%s, %s)", (a, b, expected) => {
+    expect(isEq(a, b)).toBe(expected);
+  })
+
+  it.each(decimal)("Decimal isEq(%s, %s)", (a, b, expected) => {
+    expect(isEq(a, b)).toBe(expected);
+  })
+
+});
