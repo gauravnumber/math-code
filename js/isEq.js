467e33963bf9b2a6a4c3724abbc7bf8706ba3f82
Gaurav Singh <gauravnumber@gmail.com>
2021-08-23 12:41:46 +0530

    isEq improved

--- js/isEq.js
+++ js/isEq.js
@@ -1,76 +1,104 @@
+import removeZeroFromLeft from './.internal/removeZeroFromLeft.js'
+import isNegative from './.internal/isNegative.js'
+import isDecimal from './.internal/isDecimal.js'
+import decimalPosition from './.internal/decimalPosition.js'
+import split from './.internal/split.js'
+
 export function isEq(first, second) {
 	var lengthno,
 		i,
-		firstLastIndex, secondLastIndex,
-		firstnohold, secondnohold,
-		decimalfirst, decimalsecond,
+		// firstLastIndex, secondLastIndex,
+		// firstnohold, secondnohold,
+		// decimalfirst, decimalsecond,
 		firstpos, secondpos,
 		firstslice, secondslice,
 		firsthalf, secondhalf,
 		third, thirdhalf, thirdslice;
 	third = [];
-	decimalfirst = false;
-	decimalsecond = false;
+	// decimalfirst = false;
+	// decimalsecond = false;
 	firstpos = secondpos = -1;
-	first = String(first);
-	second = String(second);
-	first = first.split("");
-	second = second.split("");
-	while (Number(first[0]) == 0)
-		first.shift();
-	while (Number(second[0]) == 0)
-		second.shift();
-	if (first[0] == "-" && second[0] == "-") {
+	// first = String(first);
+	// second = String(second);
+	// first = first.split("");
+	// second = second.split("");
+
+	first = split(first);
+	second = split(second);
+
+
+	first = removeZeroFromLeft(first).split("")
+	second = removeZeroFromLeft(second).split("")
+
+
+	// while (Number(first[0]) == 0)
+	// 	first.shift();
+	// while (Number(second[0]) == 0)
+	// 	second.shift();
+	// if (first[0] == "-" && second[0] == "-") {
+	if (isNegative(first) && isNegative(second)) {
 		first = first.slice(1);
 		second = second.slice(1);
 		first = first.join("");
 		second = second.join("");
 		third = isEq(first, second);
 		return third;
-	} else if (first[0] != "-" && second[0] == "-") {
+	} else if (!isNegative(first) && isNegative(second)) {
+		// } else if (first[0] != "-" && second[0] == "-") {
 		return false;
-	} else if (first[0] == "-" && second[0] != "-") {
+	} else if (isNegative(first) && !isNegative(second)) {
+		// } else if (first[0] == "-" && second[0] != "-") {
 		return false;
 	}
-	for (i = 0; i < first.length; i++)
-		if (first[i] == ".") {
-			decimalfirst = true;
-			firstpos = i;
-			break;
-		}
-	for (i = 0; i < second.length; i++)
-		if (second[i] == ".") {
-			decimalsecond = true;
-			secondpos = i;
-			break;
-		}
-	while (first[first.length - 1] == 0 && decimalfirst == true) {
+
+	// firstpos = decimalPosition(first)
+	// secondpos = decimalPosition(second)
+
+	// for (i = 0; i < first.length; i++)
+	// 	if (first[i] == ".") {
+	// 		decimalfirst = true;
+	// 		firstpos = i;
+	// 		break;
+	// 	}
+	// for (i = 0; i < second.length; i++)
+	// 	if (second[i] == ".") {
+	// 		decimalsecond = true;
+	// 		secondpos = i;
+	// 		break;
+	// 	}
+
+	while (first[first.length - 1] == 0 && isDecimal(first)) {
 		first.pop();
 	}
-	while (second[second.length - 1] == 0 && decimalsecond == true) {
+	while (second[second.length - 1] == 0 && isDecimal(second)) {
 		second.pop();
 	}
 	while (first[first.length - 1] == ".") {
 		first.pop();
-		decimalfirst = false;
+		// decimalfirst = false;
 	}
 	while (second[second.length - 1] == ".") {
 		second.pop();
-		decimalsecond = false;
+		// decimalsecond = false;
 	}
-	for (i = 0; i < first.length; i++)
-		if (first[i] == ".") {
-			decimalfirst = true;
-			firstpos = i;
-			break;
-		}
-	for (i = 0; i < second.length; i++)
-		if (second[i] == ".") {
-			decimalsecond = true;
-			secondpos = i;
-			break;
-		}
-	if (decimalfirst == false && decimalsecond == false) {
+
+	firstpos = decimalPosition(first)
+	secondpos = decimalPosition(second)
+
+	// for (i = 0; i < first.length; i++)
+	// 	if (first[i] == ".") {
+	// 		decimalfirst = true;
+	// 		firstpos = i;
+	// 		break;
+	// 	}
+	// for (i = 0; i < second.length; i++)
+	// 	if (second[i] == ".") {
+	// 		decimalsecond = true;
+	// 		secondpos = i;
+	// 		break;
+	// 	}
+	// if (decimalfirst == false && decimalsecond == false) {
+	if (!isDecimal(first) && !isDecimal(second)) {
 		if (first.length == second.length) {
 			lengthno = first.length;
 			for (i = 0; i < lengthno; i++) {
@@ -82,11 +110,14 @@ export function isEq(first, second) {
 		} else {
 			return false;
 		}
-	} else if (decimalfirst == true && decimalsecond == false) {
+	// } else if (decimalfirst == true && decimalsecond == false) {
+	} else if (isDecimal(first) && !isDecimal(second)) {
 		return false;
-	} else if (decimalfirst == false && decimalsecond == true) {
+	// } else if (decimalfirst == false && decimalsecond == true) {
+	} else if (!isDecimal(first) && isDecimal(second)) {
 		return false;
-	} else if (decimalfirst == true && decimalsecond == true) {
+	} else if (isDecimal(first) && isDecimal(second)) {
+	// } else if (decimalfirst == true && decimalsecond == true) {
 		firstslice = first.slice(firstpos + 1);
 		secondslice = second.slice(secondpos + 1);
 		firsthalf = first.slice(0, firstpos);
