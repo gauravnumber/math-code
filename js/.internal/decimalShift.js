15267791b9b37f230d15684a2c0b2d23b7c649c4
Gaurav Singh <gauravnumber@gmail.com>
2021-09-30 20:42:40 +0530

    removing unneccessary code

--- js/.internal/decimalShift.js
+++ js/.internal/decimalShift.js
@@ -2,9 +2,6 @@ import isDecimal from './isDecimal.js'
 import decimalPosition from './decimalPosition.js'
 import split from './split.js'
 
-import { mul } from '../mul.js'
-import { pow } from '../pow.js'
-
 const decimalShift = (value, shift) => {
   let valueDecimalPosition,
     decimalPositionTemp,
@@ -21,55 +18,35 @@ const decimalShift = (value, shift) => {
   if (shift) {
     valueDecimalPosition = decimalPosition(value)
     decimalPositionTemp = valueDecimalPosition + shift
-    // shift += valueDecimalPosition
 
     if (decimalPositionTemp < value.length) {
       value.splice(valueDecimalPosition, 1)
       if (decimalPositionTemp !== value.length) {
         value.splice(decimalPositionTemp, 0, ".")
       }
-
-      // return value.join("")
     } else {
-      // console.log('value', value)
       valueDecimalPosition = decimalPosition(value)
       lastDecimalDigit = value.length - valueDecimalPosition
       numberOfAddingZero = shift - lastDecimalDigit + 1
 
-      // console.log('valueDecimalPosition', valueDecimalPosition)
-      // console.log('decimalPositionTemp', decimalPositionTemp)
+      let numberOfAddingZeroAbs = Math.abs(numberOfAddingZero)
 
       value.splice(valueDecimalPosition, 1)
 
-      // console.log('numberOfAddingZero', numberOfAddingZero)
-
-      for (let i = 0; i < Math.abs(numberOfAddingZero); i++) {
+      for (let i = 0; i < numberOfAddingZeroAbs; i++) {
         value.push("0")
       }
 
-      // value.splice(decimalPositionTemp, 0, ".")
-
-      // console.log('numberOfAddingZero', numberOfAddingZero)
       return value.join("")
-
     }
-    // value.splice(shift, 0, ".")
 
     if (decimalPositionTemp === 0) {
-      // console.log('value', value)
-      // value.splice(decimalPositionTemp, 0, ".")
-
       value.unshift("0")
       return value.join("")
     }
 
-
     if (decimalPositionTemp < 0) {
-      // console.log('value', value)
       let decimalPositionTempAbs = Math.abs(decimalPositionTemp)
-
-      // value.splice(decimalPositionTemp, 0, ".")
-
       value.splice(decimalPosition(value), 1)
 
       for (let i = 0; i <= decimalPositionTempAbs; i++) {
@@ -80,18 +57,9 @@ const decimalShift = (value, shift) => {
       return value.join("")
     }
 
-    // console.log('decimalPositionTemp', decimalPositionTemp)
-    // value.splice(shift, 0, ".")
-    // console.log('value', value)
-
-    // value = mul(value, "100")
-    // console.log('value', value)
-    // value = mul(value, pow("10", shift))
-    // console.log('pow("10", shift)', pow("10", shift))
-
-    // return value
     return value.join("")
   }
+
   return value
 }
 
