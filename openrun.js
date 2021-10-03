9e2282df0e5bee5f11d9bebd4a7d8cbe2a611565
Gaurav Singh <gauravnumber@gmail.com>
2021-10-03 19:22:17 +0530

    removing comment in openrun.js

--- openrun.js
+++ openrun.js
@@ -16,8 +16,8 @@ import decimalShift from './js/.internal/decimalShift.js'
 import removeZeroFromRight from './js/.internal/removeZeroFromRight.js'
 // import isZero from './js/.internal/isZero.js'
 
-// const temp = singleDigitDiv("1", "1000000000000")
-// console.log(mul("23.232", temp)) 
+const temp = div("1", "1000000000000")
+console.log(mul("23.232", temp)) 
 
 // console.log(mul("2342.2323", "0.0000001"));
 // console.log(mul("2342.2323", "44768.343"));
@@ -27,96 +27,13 @@ import removeZeroFromRight from './js/.internal/removeZeroFromRight.js'
 // console.log(mul("12343.2323", "123465.12445"));
 // console.log(removeZeroFromRight("233.000"));
 // console.log(mul("25675", "234245.21323"));
-console.log(mul("12345", "12343.2323"));
+// console.log(mul("12345", "12343.2323"));
 // console.log(mul("12343.2323", "12345"));
 // console.log(isDecimal("0.0001"));
 // console.log(isDecimal("2342.2323"));
 
 // console.log(mul("212", "2"));
-// console.log(mul("2342.2323", "0.0000000000000000000001"));
-// console.log(mul("12343.2323", "100000"));
-
-// for (let index = -20; index <= 20; index++) {
-//   console.log("index:" + index + " " + decimalShift("12345", index));
-// }
-
-// console.log(div("355", "113", "100"));
-
-// console.log(decimalShift("123", "-5"));
-// console.log(decimalShift("123", "-3"));
-// console.log(decimalShift("123", "-1"));
-
-// let dividend = "1237.48956"
-// // divisorDecimalPosition = decimalPosition(divisor)
-// let dividendDecimalPosition = decimalPosition(dividend)
-
-// let dividendLastDecimalPosition = dividend.length - dividendDecimalPosition
-
-// console.log(dividendLastDecimalPosition);
-// console.log(dividendDecimalPosition);
-
-// console.log(singleDigitDiv("155060736", "12345.6"));
-// console.log(singleDigitDiv("15506073.6", "1.23456"));
-// console.log(singleDigitDiv("1550607.36", "123456"));
-// console.log(decimalShift("12133.0", "-3"));
-
-// 10945/12567
-
-// for (let i = 0; i <= 15; i++) {
-//   console.log(`${i} : ${singleDigitDiv("126789.1", "1256.7", i)}`)
-//   // console.log(`${i} : ${singleDigitDiv("123456789.1", "1223456.7", i)}`)
-//   // console.log(singleDigitDiv("126789.1", "1256.7", "2"))
-// }
-// console.log(singleDigitDiv("10945", "12567"))
-// console.log(singleDigitDiv("1234567891", "12567"))
-// console.log(singleDigitDiv("1252.32345", "212231.2212345")) // not working as expected
-
-// for (let index = -20; index <= 20; index++) {
-//   console.log("index:" + index + " " + decimalShift("12345.6789", index));
-// }
-
-// console.log(decimalShift("123456.789", "3"))  // -7 4
-
-// console.log(decimalShift("34", "0")) 
-
-// console.log(singleDigitDiv("1", "1000000")) 
-
-// console.log(pow("10", "3")) 
-
-// console.log(decimalPosition("125989.1")) 
-// console.log(decimalPosition("1255.29891")) 
-// console.log(singleDigitDiv("1252323.23231", "1.23212", "12")) 
-// console.log("2332.23".indexOf("."));
-// console.log("2332.23".lastIndexOf("."));
-
-
-// console.log(singleDigitDiv("-1251", "12")) 
-// console.log(singleDigitDiv("-6", "-12"));
-
-// console.log(singleDigitDiv("6", "12"));
-// console.log(singleDigitDiv("1251", "12")) 
-// console.log(singleDigitDiv("3", "12")) 
-
-// console.log(isLt(["6"], ["1", "2"]));
-// console.time()
-// console.log(singleDigitDiv("11257", "7", "50")) 
-// console.log(singleDigitDiv("11245", "2")) 
-// console.log(singleDigitDiv("0", "2"))
-// console.log(singleDigitDiv("12", "0")) 
-// console.log(singleDigitDiv("12", "12"))
-// console.log(singleDigitDiv("37284", "2")) // => 18642
-// console.timeEnd()
-// console.log(singleDigitDiv("11246", "2")) // => 5623
-// console.log(singleDigitDiv("14287", "7")) // => 2041
-// console.log(singleDigitDiv("3186", "3"))
-// console.log(singleDigitDiv("1242", "2"))
-// console.log('abc'.charAt(1));
-
-// console.log(singleDigitDiv("96396", "3"))
-// console.log(singleDigitDiv("8642", "2"))
-// console.log(divisibleFor("2", "2"));
-// console.log(singleDigitDiv("2", "2")); // not working now
-
+console.log(mul("2342.2323", "0.0000000000000000000001"));
 
 // console.time()
 // console.log(singleDigitDiv("1667", "1222", "1000")) // 102 digit fit my desktop screen including "."
