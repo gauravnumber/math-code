dc7c3862ca6aeab63aad18fbedb8a8e2bada9115
Gaurav Singh <gauravnumber@gmail.com>
2021-10-03 19:29:39 +0530

    write some test and clear openrun.js

--- __tests__/removeZeroFromLeft.spec.js
+++ __tests__/removeZeroFromLeft.spec.js
@@ -1,12 +1,25 @@
 import removeZeroFromLeft from '../js/.internal/removeZeroFromLeft.js'
 
-describe('removeZeroFromLeft', () => {
-  // test('should 002 -> ["2"]', () => {
-  //   expect(removeZeroFromLeft('002')).toStrictEqual(["2"])
-  // })
+// describe('removeZeroFromLeft', () => {
+//   // test('should 002 -> ["2"]', () => {
+//   //   expect(removeZeroFromLeft('002')).toStrictEqual(["2"])
+//   // })
 
-  test('should 0003 -> "3"', () => {
-    expect(removeZeroFromLeft('0003')).toBe('3')
-  })
+//   test('should 0003 -> "3"', () => {
+//     expect(removeZeroFromLeft('0003')).toBe('3')
+//   })
   
+// })
+
+describe('removeZeroFromLeft', () => {
+  const array = [
+    ["00233", "233"],
+    ["0", "0"],
+    // ["-000000", "0"],
+  ]
+
+  it.each(array)('isZero(%s)', (a, expected) => {
+    expect(removeZeroFromLeft(a)).toBe(expected)
+  })
+
 })
--- openrun.js
+++ openrun.js
@@ -9,11 +9,11 @@ import { mul } from './js/mul.js'
 import { div } from './js/div.js'
 // import singleDigitDiv from './js/singleDigitDiv.js'
 // import divisibleFor from './js/divisibleFor.js'
-// import removeZeroFromLeft from './js/.internal/removeZeroFromLeft.js'
-import decimalPosition from './js/.internal/decimalPosition.js'
-import decimalShift from './js/.internal/decimalShift.js'
+import removeZeroFromLeft from './js/.internal/removeZeroFromLeft.js'
+// import decimalPosition from './js/.internal/decimalPosition.js'
+// import decimalShift from './js/.internal/decimalShift.js'
 // import isDecimal from './js/.internal/isDecimal.js'
-import removeZeroFromRight from './js/.internal/removeZeroFromRight.js'
+// import removeZeroFromRight from './js/.internal/removeZeroFromRight.js'
 // import isZero from './js/.internal/isZero.js'
 
 const temp = div("1", "1000000000000")
@@ -61,24 +61,3 @@ console.log(mul("2342.2323", "0.0000000000000000000001"));
 // console.log(singleDigitDiv("2", "6"))
 // console.log(singleDigitDiv("5", "2"))
 // console.log(singleDigitDiv("12", "8"))
-
-
-// console.log(mul("3", "-0"));
-// console.log(mul("-0", "2"));
-
-// console.log(isZero("-0"));
-// console.log(isZero("-."));
-// console.log(isZero("4"));
-// console.log(isZero("--7"));
-
-// console.log(fact(4));
-
-// console.log(removeZeroFromLeft("-000000"));
-// console.log(removeZeroFromLeft("00233"));
-// console.log(removeZeroFromLeft("0"));
-
-// console.log(mcMulSection("0", "2"))
-// console.log(mcAddSection(['2', '4'], "2"))
-// console.log(mcMulSection(['2', '4'], "2"))
-// console.log(mcMulSection("-1224342.3", "-736527.3"))
-// console.log(typeof divisibleFor("4", "3"));
